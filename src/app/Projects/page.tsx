0380a8fd2dbe664b39f109c35a2a085c22faf65a
“Mansour” <mansournosrati69@gmail.com>
2025-05-31 19:48:31 +0330

    final Message

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
   return (
     <div className="flex flex-col items-center pt-10 bg-gray-700 h-screen text-white gap-3">
       <h2 className="text-[20px] lg:text-[30px] tracking-[5px] first-letter:font-bold mb-10">Projects</h2>
-      {allProjects.map((item, id) => (
+      {allProjects.map((item) => (
         <Link href={item.url} key={item.id} target="_blank" rel="noopener noreferrer">
           <h2 className="cursor-pointer text-white tracking-widest bg-gray-900 w-[90vw] text-center p-2 rounded-[4px]">{item.id} : {item.name}</h2>
         </Link>
